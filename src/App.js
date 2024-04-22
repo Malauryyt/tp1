0c2107504d148659437a6d2e4df0189cd04d288c
Malauryyt <110236103+Malauryyt@users.noreply.github.com>
2024-04-22 15:06:47 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { LoremIpsum } from 'react-lorem-ipsum';
+import RandomText from "./composent.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>TP1 React</h1>
+                <RandomText />
+            </header>
+
+        </div>
+    );
 }
+
+//tests pour comprendre
+function MyButton() {
+    return (
+        <button >I'm a button</button>
+    );
+}
+
+function RandomCont() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `Count: ${count}`;
+    }, [count]);
+
+    return (
+
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </div>
+    );
+}
+
+function RandomColor() {
+
+    const [color, setColor] = useState("edc4f7");
+
+    const generateColor = () => {
+        setColor(Math.random().toString(16).substr(-6))
+    };
+
+    return (<>
+        <div
+            style={{
+                height: "100vh",
+                width: "100vw",
+                backgroundColor: "#" + color,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <button
+                style={{
+                    padding: "40px",
+                    borderRadius: "10px",
+                    backgroundImage:
+                        "linear-gradient(to top, #a8edea 0%, #fed6e3 100%)",
+                    fontSize: "larger",
+                }}
+                onClick={generateColor}
+            >
+                Generate random color
+            </button>
+        </div>
+    </>
+    );
+}
+
+
+
+
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import RandomImage from './randomImage';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<App />} />
+                {/* <Route path="/image" element={<RandomImage />} /> */}
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
